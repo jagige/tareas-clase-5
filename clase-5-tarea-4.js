8d7cbb2e314f823274b261689646aa987a0f66f7
jagige <gigena_javier@hotmail.com>
2024-04-27 15:40:18 -0300

    primera parte
    Para las 3 primeras consignas se usan los ejercicios de la clase 4 (algoritmos)

--- clase-5-tarea-4.js
+++ clase-5-tarea-4.js
@@ -1,2 +1,19 @@
-// Clase 5 GitHub empieza en la hs 1
-//Banches 1:15:15
+const $numerosEnLi = document.querySelectorAll("li");
+const arrayDeNumerosEnLi = []
+
+for(let i =0; i<$numerosEnLi.length; i++){
+    arrayDeNumerosEnLi.push(parseInt($numerosEnLi[i].innerText));
+}
+
+function promedioArray(numeros){
+    let resultado = 0;
+    for(let i =0; i < numeros.length; i++){
+      resultado = resultado + numeros[i]
+    }
+    return resultado/numeros.length;
+  }
+
+console.log("el numero minimo es "+Math.min(...arrayDeNumerosEnLi));
+console.log("el numero máximo es "+Math.max(...arrayDeNumerosEnLi));
+console.log("el promedio es: "+ promedioArray(arrayDeNumerosEnLi));
+
